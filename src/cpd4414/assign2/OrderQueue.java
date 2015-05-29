62306d59c69bee31e8f5d4a0f5c1174d97c34c9d
c0655617 <c0655617@SB-N208-08.lambton.on.ca>
2015-05-29 15:20:37 -0400

    some more tests

--- src/cpd4414/assign2/OrderQueue.java
+++ src/cpd4414/assign2/OrderQueue.java
@@ -18,7 +18,9 @@
 package cpd4414.assign2;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -27,7 +29,7 @@ import java.util.Queue;
  */
 public class OrderQueue {
     Queue<Order> orderQueue = new ArrayDeque<>();
-    
+     List<Order> orderList = new ArrayList<>();
     public void add(Order order) throws NoCustomerException,NoPurchasesException{
         if(order.getCustomerId().isEmpty()&& order.getCustomerName().isEmpty()){
             throw new NoCustomerException();
@@ -42,6 +44,17 @@ public class OrderQueue {
     Order next() {
        return orderQueue.peek();
     }
+
+     void process(Order next) throws NoTimeReceivedException {
+        if (next.equals(next())) {
+        orderList.add(orderQueue.remove());
+            next.setTimeProcessed(new Date());
+        } else if (next.getTimeReceived() == null) {
+            throw new NoTimeReceivedException();
+        }
+    }
+  
+
     public class NoCustomerException extends Exception {
         public NoCustomerException(){
             super("No Customer");
@@ -53,4 +66,11 @@ public class OrderQueue {
             super("No Purchases");
         }
     }
+       public class NoTimeReceivedException extends Exception {
+
+        public NoTimeReceivedException() {
+            super("No Time Received on this Order");
+        }
+
+}
 }
